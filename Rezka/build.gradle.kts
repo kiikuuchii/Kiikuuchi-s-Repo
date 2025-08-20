e3a187926173c966b3a9d9a6d06ad0a41c9a02cf
kiikuuchii <bebra3341@gmail.com>
2025-08-20 12:52:53 +0300

    Add files via upload

---
+++ Rezka/build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+    id("com.lagradost.cloudstream3.gradle")
+}
+
+cloudstream {
+    language = "ru"
+    authors = listOf("kiikuuchii")
+    description = "Плагин поиска фильмов, сериалов и аниме с сайта rezka-ua.org"
+    status = 1
+    version = 1
+}
+
+android {
+    namespace = "com.rezka"
+    compileSdkVersion = "android-33" // Можно ставить 33 или ту, что в корневом файле
+
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 35
+    }
+	
+}
+
+    dependencies {
+        val implementation by configurations
+        val cloudstream by configurations
+        cloudstream("com.lagradost:cloudstream3:pre-release")
+        
+		implementation(kotlin("stdlib"))
+        implementation("com.github.Blatzar:NiceHttp:0.4.13")
+        implementation("org.jsoup:jsoup:1.19.1")
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.16.0")
+        implementation("com.fasterxml.jackson.core:jackson-databind:2.16.0")
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.1")
+        implementation("org.mozilla:rhino:1.8.0")
+        implementation("me.xdrop:fuzzywuzzy:1.4.0")
+        implementation("com.google.code.gson:gson:2.11.0")
+        implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.0")
+        implementation("app.cash.quickjs:quickjs-android:0.9.2")
+        implementation("com.github.vidstige:jadb:v1.2.1")
+}
