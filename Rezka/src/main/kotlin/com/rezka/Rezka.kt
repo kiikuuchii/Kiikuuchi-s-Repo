59daca0140108df01921587788f2b716c9218847
kiikuuchii <bebra3341@gmail.com>
2025-08-21 09:58:51 +0300

    Add files via upload

--- Rezka/src/main/kotlin/com/rezka/Rezka.kt
+++ Rezka/src/main/kotlin/com/rezka/Rezka.kt
@@ -2,42 +2,36 @@ package com.rezka
 
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.utils.*
+import com.lagradost.cloudstream3.MainAPI
+import com.lagradost.cloudstream3.TvType
+import com.lagradost.cloudstream3.SearchResponse
+import com.lagradost.cloudstream3.LoadResponse
+import com.lagradost.cloudstream3.utils.AppUtils
+import com.lagradost.cloudstream3.utils.loadExtractor
+import com.lagradost.cloudstream3.utils.ExtractorLink
+import com.lagradost.cloudstream3.SubtitleFile
+import org.jsoup.Jsoup
 
 class Rezka : MainAPI() {
     override var name = "Rezka"
     override var mainUrl = "https://rezka-ua.org"
     override var lang = "ru"
-    override val supportedTypes = setOf(TvType.Movie, TvType.TvSeries)
-    override val hasMainPage = false
+    override val supportedTypes = setOf(TvType.Movie, TvType.TvSeries, TvType.Anime)
 
     override suspend fun search(query: String): List<SearchResponse> {
-        // Пока тестовый результат, чтобы проверить, что плагин работает
-        return listOf(
-            newMovieSearchResponse(
-                "Тестовый фильм",
-                "https://rezka-ua.org/test",
-                TvType.Movie
-            ) {
-                this.posterUrl = null
-                this.year = 2025
-            }
-        )
-    }
+        val doc = Jsoup.connect("$mainUrl/index.php?do=search&subaction=search&q=$query").get()
+        val results = mutableListOf<SearchResponse>()
 
-    override suspend fun load(url: String): LoadResponse {
-        return newMovieLoadResponse("Тестовый фильм", url, TvType.Movie, url) {
-            this.posterUrl = null
-            this.year = 2025
-            this.plot = "Это заглушка для проверки плагина."
+        for (el in doc.select(".b-content__inline_item")) {
+            val title = el.selectFirst(".b-content__inline_item-link")?.text() ?: continue
+            val url = el.selectFirst("a")?.attr("href") ?: continue
+            val poster = el.selectFirst("img")?.attr("src")
+            results.add(
+                newMovieSearchResponse(title, url, TvType.Movie) {
+                    this.posterUrl = poster
+                }
+            )
         }
-    }
-
-    override suspend fun loadLinks(
-        data: String,
-        isCasting: Boolean,
-        subtitleCallback: (SubtitleFile) -> Unit,
-        callback: (ExtractorLink) -> Unit
-    ): Boolean {
-        return false
+        return results
     }
 }
