531c99f43cbce6838f7a6d0233ce29daf45e7b2b
kiikuuchii <bebra3341@gmail.com>
2025-08-21 09:39:34 +0300

    Add files via upload

--- Rezka/build.gradle.kts
+++ Rezka/build.gradle.kts
@@ -1,27 +1,28 @@
-import org.jetbrains.kotlin.konan.properties.Properties
-
+// use an integer for version numbers
 version = 451
 
-android {
-   buildFeatures {
-       buildConfig = true
-	}
-	defaultConfig {
-	    val properties = Properties()
-		properties.load(project.rootProject.file("local.properties").inputStream())
-   }
-}
 
 cloudstream {
-    language = "ru"
+    // All of these properties are optional, you can safely remove them
+
+    description = "Rezka Plugin for Cloudstream"
+    language    = "ru"
     authors = listOf("kiikuuchii")
-    description = "Плагин поиска фильмов, сериалов и аниме с сайта rezka-ua.org"
-    status = 1
-	tvTypes = listOf(
-	      "Movie",
-		  "TvSeries",
-		  "Anime",
-		  )
-    
-	isCrossPlatform = true
+
+    /**
+    * Status int as the following:
+    * 0: Down
+    * 1: Ok
+    * 2: Slow
+    * 3: Beta only
+    * */
+    status = 1 // will be 3 if unspecified
+
+    // List of video source types. Users are able to filter for extensions in a given category.
+    // You can find a list of available types here:
+    // https://recloudstream.github.io/cloudstream/html/app/com.lagradost.cloudstream3/-tv-type/index.html
+    tvTypes = listOf("Movie","TvSeries")
+    iconUrl = "https://raw.githubusercontent.com/phisher98/TVVVV/refs/heads/main/Icons/Coflix.png"
+
+    isCrossPlatform = true
 }
